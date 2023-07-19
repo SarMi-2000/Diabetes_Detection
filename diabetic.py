bae7636ba5cf4abac2900341c6d7b582abc75f9f
Sarmistha Bera <68506912+SarMi-2000@users.noreply.github.com>
2023-07-19 14:49:57 +0530

    Update diabetic.py

--- diabetic.py
+++ diabetic.py
@@ -1,29 +1,56 @@
 import numpy as np
 import pandas as pd
-data=pd.read_csv("Diabetes.csv")
+
+# Read the dataset from the CSV file
+data = pd.read_csv("Diabetes.csv")
+
+# Display information about the dataset
 data.info()
-data.isnull.sum()
-X=data.iloc[:,:-1]
-y=data.iloc[:,-1]
+
+# Check for any missing values
+data.isnull().sum()
+
+# Separate the features (X) and target variable (y)
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+# Split the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
-X_train,X_text,y_train,y_text=train_test_split(X,y,random_state=0,test_size=25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=25)
+
+# Train a Random Forest classifier
 from sklearn.ensemble import RandomForestClassifier
-Classfier=RandomForestClassifier(n_estimators=8,criterion='entropy',random_state=0)
-Classfier.fit(X_train,y_train)
-y_pred=Classfier.predict(X_text)
+classifier = RandomForestClassifier(n_estimators=8, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = classifier.predict(X_test)
+
+# Calculate the accuracy of the Random Forest classifier
 from sklearn.metrics import accuracy_score
-acc_randomf=round(accuracy_score(y_pred,y_text),2)*100
-print("Accuracy Random Forest Classifier: ",acc_randomf)
+acc_randomf = round(accuracy_score(y_pred, y_test), 2) * 100
+print("Accuracy Random Forest Classifier:", acc_randomf)
+
+# Train a Logistic Regression classifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,r2_score,classification_report
-logreg=LogisticRegression(solver='lbfgs',max_iter=1000)
-logreg.fit(X_train,y_train)
-y_pred=logreg.predict(X_text)
-acc_logreg=round(accuracy_score(y_pred,y_text),2)*100
-print("Accuracy Logistic Regression: ",acc_logreg)
+logreg = LogisticRegression(solver='lbfgs', max_iter=1000)
+logreg.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = logreg.predict(X_test)
+
+# Calculate the accuracy of the Logistic Regression classifier
+acc_logreg = round(accuracy_score(y_pred, y_test), 2) * 100
+print("Accuracy Logistic Regression:", acc_logreg)
+
+# Train a K Neighbors classifier
 from sklearn.neighbors import KNeighborsClassifier
-knn=KNeighborsClassifier(n_neighbors=8)
-knn.fit(X_train,y_train)
-y_pred=knn.predict(X_test)
-acc_knn=round(accuracy_score(y_pred,y_text),2)*100
-print("Accuracy K Neighbors Classifier: ",acc_knn)
+knn = KNeighborsClassifier(n_neighbors=8)
+knn.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = knn.predict(X_test)
+
+# Calculate the accuracy of the K Neighbors classifier
+acc_knn = round(accuracy_score(y_pred, y_test), 2) * 100
+print("Accuracy K Neighbors Classifier:", acc_knn)
